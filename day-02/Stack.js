2ae6f1742b046511d6cb17c41360bf44b8d6512e
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-01-31 09:59:32 -0800

    solve stack question

--- day-02/Stack.js
+++ day-02/Stack.js
@@ -1,3 +1,38 @@
-class Stack {}
+class Stack {
+  #list = [];
+
+  constructor(initialList) {
+    if (initialList) this.#list = initialList;
+  }
+
+  push(item) {
+    return this.#list.push(item);
+  }
+
+  pop() {
+    if (this.#list.length === 0) {
+      return null;
+    } else return this.#list.pop();
+  }
+
+  peek() {
+    return this.#list[this.#list.length - 1];
+  }
+
+}
 
 module.exports = { Stack };
+
+
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push('llama');
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
